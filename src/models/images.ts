9188d58a684253f818dedab89166f8ebe8243f9c
Miqueias.Moreira <miqueias.moreira@villefortconsulting.com>
2025-08-25 18:01:38 -0300

    feat: 🖼️ adicionar recursos e utilitários para imagens e classes
    - Criado arquivo `images.ts` para centralizar gerenciamento de imagens (flags e foto de perfil).
    - Adicionado utilitário `cn` para facilitar a manipulação de classes com `clsx` e `tailwind-merge`.
    - Ajustados nomes de arquivos em `models` para consistência.

---
+++ src/models/images.ts
@@ -0,0 +1,13 @@
+
+// Flags
+import PROFILE_PHOTO from '../../public/profile-photo.png';
+import FLAG_BR from '../../public/locales/pt-BR/pt-BR.png';
+import FLAG_US from '../../public/locales/en-US/en-US.png';
+import FLAG_ES from '../../public/locales/es-ES/es-ES.png';
+
+export const Images = {
+    PROFILE_PHOTO,
+    FLAG_BR,
+    FLAG_US,
+    FLAG_ES,
+};
