84d0c1bcf35209bd208fd96519d192c9c0718ce4
Miqueias.Moreira <miqueias.moreira@villefortconsulting.com>
2025-08-25 14:49:43 -0300

    ci: 🔄 ajuste no build para exportação estática
    - Adicionado `next export` ao processo de build no workflow do GitHub Actions.
    - Atualizado o script de build no `package.json` para incluir exportação estática.
    - Configurado `next.config.ts` com suporte para geração de arquivos estáticos:
      - Adicionado `output: "export"`.
      - Imagens configuradas como não otimizadas.
      - Definidos `basePath` e `assetPrefix`.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export", // força o Next.js a gerar arquivos estáticos em ./out
+    images: {
+        unoptimized: true, // desativa otimização de imagens (não suportada no Pages)
+    },
+    basePath: "/miqueias-rocha.dev" ,
+    assetPrefix: "/miqueias-rocha.dev/",
 };
 
 export default nextConfig;
